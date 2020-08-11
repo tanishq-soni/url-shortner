547ce84b7a6ef4d5955b7bf2250112caa65a7f00
Tanishq Soni <55662221+tanishq-soni@users.noreply.github.com>
2020-08-11 23:08:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     os.mkdir('dist')
 
     with open('dist/CNAME', 'w') as f:
-        f.write('tanishq-soni.github.io/url-shortner')
+        f.write('fade.cf')
 
     for link in links:
         html_document = html.format(url=link['url'])
