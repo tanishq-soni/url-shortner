c4fbe748d154fa2108a43df7a4d822ad24156134
Tanishq Soni <tanishqsoni49777@gmail.com>
2020-08-11 22:30:26 +0530

    update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     os.mkdir('dist')
 
     with open('dist/CNAME', 'w') as f:
-        f.write('tshr.me')
+        f.write('tanishq-soni.github.io/url-shortner')
 
     for link in links:
         html_document = html.format(url=link['url'])
